bdc4284d82eec626fae6013010f0aad94c33bcd5
Achint Sandhu <sandhu@7theta.com>
2022-01-11 15:48:08 -0500

    Format xml file

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
 lein with-profile -user run -m nvd.task.check "" "$(cd $1; lein with-profile -user,-dev classpath)"
+sed -i 's/<?xml version="1.0" encoding="UTF-8"?>/<?xml version="1.0" encoding="UTF-8"?>\n/g' target/nvd/dependency-check-junit.xml
