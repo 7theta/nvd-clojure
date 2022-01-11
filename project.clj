59966ac609f3f2bfa5b607f48c36869149761fc5
Achint Sandhu <sandhu@7theta.com>
2022-01-11 12:38:55 -0500

    initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject com.7theta/nvd-clojure "0.1.0"
+  :description "shell project for nvd-clojure"
+  :url "https://github.com/7theta/nvd-clojure"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[nvd-clojure/nvd-clojure "2.1.0"]]
+  :scm {:name "git"
+        :url "https://github.com/7theta/nvd-clojure"})
